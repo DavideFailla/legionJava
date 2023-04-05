b98a491d1613ab16437070a56eed93c6f491e3f4
VirgilioDElia <vrseven@icloud.com>
2023-04-05 17:37:37 +0200

    implements CourseEditionRepository

---
+++ src/main/java/org/generation/italy/legion/dtos/CourseEditionDto.java
@@ -0,0 +1,66 @@
+package org.generation.italy.legion.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.generation.italy.legion.model.entities.Classroom;
+import org.generation.italy.legion.model.entities.Course;
+import org.generation.italy.legion.model.entities.CourseEdition;
+import org.generation.italy.legion.model.entities.EditionModule;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+public class CourseEditionDto {
+    private long id;
+    @JsonIgnore
+    private Course course;
+    private String startedAt;
+    private double cost;
+
+    public CourseEditionDto(long id, Course course, String startedAt, double cost) {
+        this.id = id;
+        this.course = course;
+        this.startedAt = startedAt;
+        this.cost = cost;
+    }
+
+    public static CourseEditionDto fromEntity(CourseEdition cE){
+        return new CourseEditionDto(cE.getId(), cE.getCourse(),cE.getStartedAt().toString(),cE.getCost());
+    }
+    public static Iterable<CourseEditionDto> fromEntityIterable(Iterable<CourseEdition> cE){
+        return StreamSupport.stream(cE.spliterator(), false)
+                .map(s -> CourseEditionDto.fromEntity(s)).toList();
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getStartedAt() {
+        return startedAt;
+    }
+
+    public void setStartedAt(String startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
--- src/main/java/org/generation/italy/legion/model/data/abstractions/CourseEditionRepository.java
+++ src/main/java/org/generation/italy/legion/model/data/abstractions/CourseEditionRepository.java
@@ -1,19 +1,38 @@
 package org.generation.italy.legion.model.data.abstractions;
 
 import org.generation.italy.legion.model.entities.CourseEdition;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface CourseEditionRepository {
-    double getTotalCost();
-    Optional<CourseEdition> findMostExpensive();
-    double findAverageCost();
-    Iterable<Double> findAllDuration();
-    Iterable<CourseEdition> findByCourse(long courseId);
-    Iterable<CourseEdition> findByCourseTitleAndPeriod(String titlePart,
-                                                       LocalDate startAt, LocalDate endAt);
-    Iterable<CourseEdition> findMedian();
-    Optional<Double> getCourseEditionCostMode();
+public interface CourseEditionRepository extends CrudRepository<CourseEdition, Long> {
+      @Query("select sum(ce.cost) from CourseEdition ce")
+      double getTotalCost();  //ok
+      @Query(""" 
+              from CourseEdition ce
+              order by ce.cost desc
+              limit 1
+              """)
+      Optional<CourseEdition> findMostExpensive(); //ok
+      @Query("""
+              select sum(ce.cost)/count(ce)
+              from CourseEdition ce
+              """)
+      double findAverageCost();  //ok
+      @Query("""
+              select c.duration
+              from CourseEdition ce join Course c on ce.c.id
+              """)
+      Iterable<Double> findAllDuration();
+
+      Iterable<CourseEdition> findByCourseId (long courseId);  //ok
+
+      Iterable<CourseEdition> findByCourseTitleAndStartedAtBetween(String titlePart, LocalDate startAt, LocalDate endAt);  //ok
+
+
+//    Iterable<CourseEdition> findMedian();
+//    Optional<Double> getCourseEditionCostMode();
 
 }
